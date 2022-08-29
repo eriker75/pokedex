44bae117409c7d829b6ee5da20fa22e03f159b61
eriker75 <eribertmarquez75@gmail.com>
2022-08-29 18:20:39 -0400

    initial commit nestjs mongo pokedex project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Study notes
+
+* nest g res pokemon --no-spec
+* yarn add @nestjs/mapped-types
+* yarn add typescript@4.7.4
+* yarn add  -D  @nestjs/serve-static
+* docker-compose up -d
---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    PokemonModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
